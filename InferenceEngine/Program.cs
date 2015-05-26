15498e9094fa5690c8ad88a01a78e0c39d0bb562
JMalmberg <joakim.malmberg@gmail.com>
2015-05-26 19:46:06 +1000

    added the beginnings of a statement interpreter *needs much work

--- InferenceEngine/Program.cs
+++ InferenceEngine/Program.cs
@@ -12,5 +12,26 @@ namespace InferenceEngine
         {
           //hello world
         }
+
+        void InstantiateTerms(List<string> statements)
+        {
+            string sTemp;
+            List<string> forRemoval = new List<string>(); 
+
+            while (statements.Any())//until all statements have been analysed
+            {
+                foreach (string s in statements) //examine every string in the list of statements
+                {
+                    sTemp = s.Replace(" ", ""); //remove spaces
+                    string[] delimiters = { "=>", "&" };
+                    string[] names = sTemp.Split((delimiters), StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach(string s2 in names)
+                    {
+                        Term Term = new Term(s2, false); //somehow name each new term after the string. might have to create a 2d list.
+                    }
+                }
+            }
+        }
     }
 }
