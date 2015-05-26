35d31b5f29a303295e4a2c1bbd07a6ec86fc51cb
JMalmberg <joakim.malmberg@gmail.com>
2015-05-26 18:50:36 +1000

    added Term class

---
+++ InferenceEngine/Term.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine
+{
+    class Term
+    {
+        public bool entailed;
+        public string name;
+    
+        public Term()
+        {
+            entailed = false;
+            name = null;
+        }
+
+        public Term(string n, bool entail)
+        {
+            name = n;
+            entailed = entail;
+        }
+    }
+}
