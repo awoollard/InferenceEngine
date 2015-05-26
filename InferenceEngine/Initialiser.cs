25cd4a46272a0f92bdea4c7a8ad26517bbe34023
awoollard <awoollard@github.com>
2015-05-26 20:22:56 +1000

    Added the untracked files - oops!

---
+++ InferenceEngine/Initialiser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine
+{
+    class Initialiser
+    {
+        public Initialiser()
+        {
+            // Empty constructor
+        }
+
+        public void parseFile(String method, String fileName)
+        {
+            KnowledgeBase knowledgeBase = new KnowledgeBase();
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+            String fileLine;
+            while ((fileLine = file.ReadLine()) != null)
+            {
+                switch (fileLine)
+                {
+                    case "TELL":
+                        // Read the next line
+                        fileLine = file.ReadLine();
+
+                        // Feed the knowledge-base
+                        knowledgeBase.tell(fileLine);
+                        break;
+                    case "ASK":
+                        // Read the next line
+                        fileLine = file.ReadLine();
+
+                        // Query the knowledge-base
+                        knowledgeBase.query(fileLine, method);
+                        break;
+                    default:
+                        // Do nothing
+                        break;
+                }
+            }
+            file.Close();
+        }
+    }
+}
---
+++ InferenceEngine/KnowledgeBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine
+{
+    class KnowledgeBase
+    {
+        // Bunch of variables stored privately here... ?
+        public KnowledgeBase()
+        {
+
+        }
+
+        public bool tell(String tellStatement)
+        {
+            // Logic for querying here
+            // Maybe this.InstantiateTerms(tellStatement); goes here or something
+            return true;
+        }
+        public bool query(String queryString, String method)
+        {
+            // Logic for querying here
+            return true;
+        }
+
+        public List<Term> InstantiateTerms(List<string> statements)//returns a list of all terms in a list
+        {
+            string sTemp;
+            List<string> forRemoval = new List<string>();
+            List<Term> Terms = new List<Term>();
+
+            while (statements.Any())//until all statements have been analysed
+            {
+                foreach (string s in statements) //examine every string in the list of statements
+                {
+                    sTemp = s.Replace(" ", ""); //remove spaces
+                    string[] delimiters = { "=>", "&" };
+                    string[] names = sTemp.Split((delimiters), StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string s2 in names)
+                    {
+                        Terms.Add(new Term(s2, false));
+                    }
+                }
+            }
+            return Terms;
+        }
+    }
+}
